b38c5613b0e6450ee1912e97193388f5de28fec2
Yilber Patiño <yilberesaupm@ufps.edu.co>
2022-11-18 15:07:06 -0500

    Login Screen

--- src/modules/auth/routes/index.js
+++ src/modules/auth/routes/index.js
@@ -1,17 +1,15 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Register from '../screens/register/register';
-import Login from '../screens/login/login';
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from "../screens/register/register";
+import Login from "../screens/login/login";
 
 const Stack = createNativeStackNavigator();
 
 export function AuthStack() {
-    return (
-      <Stack.Navigator screenOptions={{headerShown: true}}>
-        <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+    </Stack.Navigator>
+  );
 }
-  
