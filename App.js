5c8fd8c46395b8e2f04768b5439c626215cf1560
Yilber Patiño <yilberesaupm@ufps.edu.co>
2022-11-18 15:02:53 -0500

    Login Page

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthStack } from './src/modules/auth/routes';
-import { FarmerStack } from './src/modules/farmer/routes';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthStack } from "./src/modules/auth/routes";
+import { FarmerStack } from "./src/modules/farmer/routes";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: true}}>
-          <Stack.Screen name="AuthStack" component={ AuthStack } />
-          <Stack.Screen name="FarmerStack" component={ FarmerStack } />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="AuthStack" component={AuthStack} />
+        <Stack.Screen name="FarmerStack" component={FarmerStack} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/modules/auth/screens/login/login.js
+++ src/modules/auth/screens/login/login.js
@@ -1,10 +1,136 @@
-import { Pressable, SafeAreaView  } from "react-native";
-import { Text } from "react-native-paper";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+  Dimensions,
+  View,
+} from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+import Constants from "expo-constants";
+import React from "react";
+const logo = require("../../../../../assets/logo-vertical-white.png");
+const image = require("../../../../../assets/image-background.jpg");
+const ScreenHeight = Dimensions.get("window").height;
+export default function Login({ navigation }) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={styles.imageBackground}
+        source={image}
+        resizeMode={"cover"}
+      >
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={logo} />
+        </View>
+        <View style={styles.form}>
+          <Text style={styles.formTitle} variant="headlineMedium">
+            Iniciar sesion
+          </Text>
 
-export default function Login({navigation}){
-    return <SafeAreaView style={{flex: 1 , justifyContent: 'center', alignItems: 'center'}}>
-        <Pressable onPress={() => navigation.replace('FarmerStack')}>
-            <Text variant="displaySmall"> Ingresar</Text>
-        </Pressable>
+          <View>
+            <TextInput
+              style={styles.inputGroup}
+              textColor="#063C2D"
+              activeUnderlineColor="#063C2D"
+              label="Email"
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+            />
+            <TextInput
+              style={styles.inputGroup}
+              textColor="#063C2D"
+              activeUnderlineColor="#063C2D"
+              label="Password"
+              value={password}
+              onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+
+          <View>
+            <Button
+              style={styles.formButton}
+              buttonColor="#15A249"
+              mode="contained"
+              onPress={() => navigation.replace("FarmerStack")}
+            >
+              <Text style={styles.text}>Ingresar</Text>
+            </Button>
+          </View>
+          <View style={styles.formTextGroup}>
+            <Text
+              style={{
+                color: "#115F49",
+                marginRight: 5,
+              }}
+              variant="bodyLarge"
+            >
+              ¿No tienes cuenta?
+            </Text>
+            <Text
+              style={{
+                color: "#063C2D",
+              }}
+              variant="bodyLarge"
+            >
+              Registrate
+            </Text>
+          </View>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
-} 
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "white",
+  },
+  imageBackground: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: ScreenHeight,
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  logo: {
+    resizeMode: "center",
+    width: 100,
+    height: 200,
+  },
+  form: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 31,
+    borderBottomEndRadius: 0,
+    borderBottomStartRadius: 0,
+  },
+  formTitle: {
+    color: "#063C2D",
+    alignSelf: "center",
+    fontWeight: "700",
+  },
+  inputGroup: {
+    marginTop: 15,
+    backgroundColor: "#F7F5F8",
+  },
+  formButton: {
+    marginVertical: 20,
+    paddingVertical: 10,
+  },
+  formTextGroup: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+});
