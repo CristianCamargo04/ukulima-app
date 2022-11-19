0600d95e40d9af9e9758befef85e71f677f716e3
CristianCamargo04 <cristianfernandocc@ufps.edu.co>
2022-11-18 22:51:10 -0500

    'ViewSFarmer Screen' has created

--- src/modules/farmer/routes/index.js
+++ src/modules/farmer/routes/index.js
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterFarmer from '../screens/registerFarmer/registerFarmer';
 import ListFarmer from '../screens/listFarmer/listFarmer';
+import ViewFarmer from '../screens/viewFarmer/viewFarmer';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export function FarmerStack() {
     return (
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="ListFarmer" component={ListFarmer} />
+        <Stack.Screen name="ViewFarmer" component={ViewFarmer} />
         <Stack.Screen name="RegisterFarmer" component={RegisterFarmer} />
       </Stack.Navigator>
     );
