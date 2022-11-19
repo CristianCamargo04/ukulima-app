464d3b4ce345a3496ea61e2d205c17ee4661358a
Yilber Patiño <yilberesaupm@ufps.edu.co>
2022-11-19 09:36:56 -0500

    scroll in register screen (#5)
    * Login Page
    
    * Register screen
    
    * Fix scroll in register screen

--- src/modules/auth/screens/register/register.js
+++ src/modules/auth/screens/register/register.js
@@ -6,7 +6,11 @@ import {
   Dimensions,
   View,
   Pressable,
-  TextInputMask,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+  Keyboard,
+  ScrollView,
 } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
@@ -40,128 +44,137 @@ export default function Register({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground
-        style={styles.imageBackground}
-        source={image}
-        resizeMode={"cover"}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
       >
-        <View style={styles.logoContainer}>
-          <Image style={styles.logo} source={logo} />
-        </View>
-        <View style={styles.form}>
-          <Text style={styles.formTitle} variant="headlineMedium">
-            Registrate
-          </Text>
-
-          <View>
-            <TextInput
-              style={styles.inputGroup}
-              textColor="#063C2D"
-              activeUnderlineColor="#063C2D"
-              label="Nombres"
-              value={email}
-              onChangeText={(email) => setEmail(email)}
-            />
-            <TextInput
-              style={styles.inputGroup}
-              textColor="#063C2D"
-              activeUnderlineColor="#063C2D"
-              label="Apellidos"
-              value={email}
-              onChangeText={(email) => setEmail(email)}
-            />
-            <TextInput
-              style={styles.inputGroup}
-              textColor="#063C2D"
-              activeUnderlineColor="#063C2D"
-              label="Correo electronico"
-              value={email}
-              keyboardType={"email-address"}
-              onChangeText={(email) => setEmail(email)}
-            />
-            <View style={styles.inputGroupTwo}>
-              <TextInput
-                style={styles.input50}
-                textColor="#063C2D"
-                activeUnderlineColor="#063C2D"
-                label="Telefono"
-                value={email}
-                keyboardType={"numeric"}
-                onChangeText={(email) => setEmail(email)}
-              />
-              <TextInput
-                style={styles.input50}
-                textColor="#063C2D"
-                activeUnderlineColor="#063C2D"
-                label="Tipo Identificacion"
-                value={email}
-                onChangeText={(email) => setEmail(email)}
-              />
-            </View>
-            <TextInput
-              style={styles.inputGroup}
-              textColor="#063C2D"
-              activeUnderlineColor="#063C2D"
-              label="Numero de identificacion"
-              value={email}
-              keyboardType={"numeric"}
-              onChangeText={(email) => setEmail(email)}
-            />
-            <View style={styles.inputGroupTwo}>
-              <View style={styles.inputDate}>
-                <Text>{date.toDateString()}</Text>
-                <Button
-                  textColor="#063C2D"
-                  icon={"calendar"}
-                  onPress={showDatepicker}
-                ></Button>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView>
+            <ImageBackground
+              style={styles.imageBackground}
+              source={image}
+              resizeMode={"cover"}
+            >
+              <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={logo} />
               </View>
+              <View style={styles.form}>
+                <Text style={styles.formTitle} variant="headlineMedium">
+                  Registrate
+                </Text>
 
-              <TextInput
-                style={styles.input50}
-                textColor="#063C2D"
-                activeUnderlineColor="#063C2D"
-                label="Contraseña"
-                value={password}
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-              />
-            </View>
-          </View>
+                <View>
+                  <TextInput
+                    style={styles.inputGroup}
+                    textColor="#063C2D"
+                    activeUnderlineColor="#063C2D"
+                    label="Nombres"
+                    value={email}
+                    onChangeText={(email) => setEmail(email)}
+                  />
+                  <TextInput
+                    style={styles.inputGroup}
+                    textColor="#063C2D"
+                    activeUnderlineColor="#063C2D"
+                    label="Apellidos"
+                    value={email}
+                    onChangeText={(email) => setEmail(email)}
+                  />
+                  <TextInput
+                    style={styles.inputGroup}
+                    textColor="#063C2D"
+                    activeUnderlineColor="#063C2D"
+                    label="Correo electronico"
+                    value={email}
+                    keyboardType={"email-address"}
+                    onChangeText={(email) => setEmail(email)}
+                  />
+                  <View style={styles.inputGroupTwo}>
+                    <TextInput
+                      style={styles.input50}
+                      textColor="#063C2D"
+                      activeUnderlineColor="#063C2D"
+                      label="Telefono"
+                      value={email}
+                      keyboardType={"numeric"}
+                      onChangeText={(email) => setEmail(email)}
+                    />
+                    <TextInput
+                      style={styles.input50}
+                      textColor="#063C2D"
+                      activeUnderlineColor="#063C2D"
+                      label="Tipo Identificacion"
+                      value={email}
+                      onChangeText={(email) => setEmail(email)}
+                    />
+                  </View>
 
-          <View>
-            <Button
-              style={styles.formButton}
-              buttonColor="#15A249"
-              mode="contained"
-              onPress={() => navigation.replace("FarmerStack")}
-            >
-              <Text style={styles.text}>Continuar</Text>
-            </Button>
-          </View>
-          <View style={styles.formTextGroup}>
-            <Text
-              style={{
-                color: "#115F49",
-                marginRight: 5,
-              }}
-              variant="bodyLarge"
-            >
-              Ya tengo cuenta.
-            </Text>
-            <Pressable onPress={() => navigation.navigate("Login")}>
-              <Text
-                style={{
-                  color: "#063C2D",
-                }}
-                variant="bodyLarge"
-              >
-                Iniciar sesion
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-      </ImageBackground>
+                  <View style={styles.inputGroupTwo}>
+                    <TextInput
+                      style={styles.input50}
+                      textColor="#063C2D"
+                      activeUnderlineColor="#063C2D"
+                      label="# identificacion"
+                      value={email}
+                      keyboardType={"numeric"}
+                      onChangeText={(email) => setEmail(email)}
+                    />
+                    <View style={styles.inputDate}>
+                      <Text>{date.toDateString()}</Text>
+                      <Button
+                        textColor="#063C2D"
+                        icon={"calendar"}
+                        onPress={showDatepicker}
+                      ></Button>
+                    </View>
+                  </View>
+                  <TextInput
+                    style={styles.inputGroup}
+                    textColor="#063C2D"
+                    activeUnderlineColor="#063C2D"
+                    label="Contraseña"
+                    value={password}
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                  />
+                </View>
+
+                <View>
+                  <Button
+                    style={styles.formButton}
+                    buttonColor="#15A249"
+                    mode="contained"
+                    onPress={() => navigation.replace("FarmerStack")}
+                  >
+                    <Text style={styles.text}>Continuar</Text>
+                  </Button>
+                </View>
+                <View style={styles.formTextGroup}>
+                  <Text
+                    style={{
+                      color: "#115F49",
+                      marginRight: 5,
+                    }}
+                    variant="bodyLarge"
+                  >
+                    Ya tengo cuenta.
+                  </Text>
+                  <Pressable onPress={() => navigation.navigate("Login")}>
+                    <Text
+                      style={{
+                        color: "#063C2D",
+                      }}
+                      variant="bodyLarge"
+                    >
+                      Iniciar sesion
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </ImageBackground>
+          </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -208,8 +221,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingVertical: 10,
     paddingStart: 15,
-    borderTopStartRadius: 10,
-    borderTopEndRadius: 10,
+    borderTopStartRadius: 5,
+    borderTopEndRadius: 5,
+    width: "49%",
   },
   inputGroup: {
     marginTop: 15,
